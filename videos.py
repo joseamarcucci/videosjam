eacf228854b096e4ceb5687c0aae1b1f6e98ef17
joseamarcucci <74363457+joseamarcucci@users.noreply.github.com>
2021-05-09 15:47:57 -0300

    Update videos.py

--- videos.py
+++ videos.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 import pytube #pip install pytube
-videos = pytube.Playlist("https://www.youtube.com/playlist?list=PLlRFEj9H3Oj7Oj3ndXmNS1FFOUyQP-gEa")
+lista=st.text_input
+videos = pytube.Playlist(lista)
 ctos=len(videos)
 todos={'Link':videos}
 df = pd.DataFrame(todos)
